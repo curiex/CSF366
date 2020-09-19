10a346d8df2e2172f43a85ec42cf6a6512674425
curiex <vishnupillai1999@gmail.com>
2020-09-19 19:58:28 +0530

    new

--- Cores Decomposition/cores.cpp
+++ Cores Decomposition/cores.cpp
@@ -21,11 +21,11 @@ vector<int> cores(vector<int> adj[],int v){
 	
 	vector<int> solution;
 	//array with degree of every vertex
-	int degree[v];
+	int degree[v+1];
 	
 	//calculating degree of every vertex and also calculating maximum degree
 	int md=0;
-	for(int i=0;i<v;i++){
+	for(int i=1;i<=v;i++){
 		int m=0;
 		m=adj[i].size();
 		degree[i]=m;
@@ -39,7 +39,7 @@ vector<int> cores(vector<int> adj[],int v){
 	for(int i=0;i<=md;i++){
 		bin[i]=0;
 	}
-	for(int i=0;i<v;++i){
+	for(int i=1;i<=v;++i){
 		bin[degree[i]]++;
 	}
 	
@@ -50,9 +50,9 @@ vector<int> cores(vector<int> adj[],int v){
 		start+=num;
 	}
 	
-	int pos[v];
-	int vert[v];
-	for(int i=0;i<v;++i){
+	int pos[v+1];
+	int vert[v+1];
+	for(int i=1;i<=v;++i){
 		pos[i]=bin[degree[i]];
 		vert[pos[i]]=i;
 		bin[degree[i]]++;
@@ -65,7 +65,7 @@ vector<int> cores(vector<int> adj[],int v){
 	
 	//algorithm
 	int n,w,pu,du,pw;
-	for(int i=0;i<v;i++){
+	for(int i=1;i<=v;i++){
 		n=vert[i];
 		vector<int> neigh=neighbours(n,adj);
 		for(int u:neigh){
@@ -85,7 +85,7 @@ vector<int> cores(vector<int> adj[],int v){
 			}
 		}
 	}
-	for (int i=0;i<v;i++){
+	for (int i=0;i<=v;i++){
 		solution.push_back(degree[i]);
 	}
 	return solution;
@@ -95,19 +95,41 @@ int main(){
 	freopen( "input.txt", "r" , stdin);
 	freopen( "output.txt", "w" , stdout);
 	
+	//edges
 	int v;
 	cin >> v;
+	
+	//nodes
 	int n=204473;
-	vector<int> adj[n];
+	
+	vector<int> adj[n+1];
+	vector<int> arr(n+1);
+	for(int i=0;i<=n;i++){
+		arr[i]=0;
+	}
+	int count = 1;
 	for(int i=0;i<v;i++){
 		int x,y;
 		cin >> x >> y;
+		if(arr[x]==0){
+			x=count;
+			arr[x]=count;
+			count++;
+		}
+		if(arr[y]==0){
+			y=count;
+			arr[y]=count;
+			count++;
+		}
 		addEdge(adj,x,y);
 	}
 	
 	vector<int> ver=cores(adj,n);
-	for(int i:ver)
-		cout << i << endl;
+	for(int i=0;i<=n;i++){
+		if(ver[i]==0)
+			continue;
+		cout << i << " - "<< ver[i]<< endl;
+	}
 	return 0;
 }
 
